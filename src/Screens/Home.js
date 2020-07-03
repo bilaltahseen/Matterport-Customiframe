b1da6107fc4da614b05970366ad51f9991861c1c
BuildTools <bilaltehseen@gmail.com>
2020-07-03 16:34:46 +0500

    Fixed Version

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -7,13 +7,18 @@ import { storeContext } from '../Context/StoreContext';
 const Home = (props) => {
   const data = useQuery();
   const [state] = React.useContext(storeContext);
+  const version = useQuery().get('version');
+  const url =
+    version === 'default'
+      ? `http://my.matterport.com/show/?${data}&play=${state.playbtn}`
+      : `http://mpembed.com/show/?${data}&play=${state.playbtn}`;
 
   return (
     <div style={{ height: '100vh' }} className='resp-container'>
       <iframe
         title='Matterport'
         style={{ backgroundColor: 'black' }}
-        src={`https://mpembed.com/show/?${data}&play=${state.playbtn}`}
+        src={url}
         frameBorder={0}
         allowFullScreen
         allow='xr-spatial-tracking'
